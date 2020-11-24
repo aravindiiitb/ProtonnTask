12010f4aba5c85c5f562b85ae3217a64e3a1369c
aravindiiitb <konakondla.sak@iiit.org>
2020-11-25 00:06:58 +0530

    removed an unused method

--- src/com/protonn/ProtonnUtils.java
+++ src/com/protonn/ProtonnUtils.java
@@ -68,12 +68,6 @@ public class ProtonnUtils {
         return dec_value;
     }
 
-    public static byte[] longToBytes(long x) {
-        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
-        buffer.putLong(0, x);
-        return buffer.array();
-    }
-
     public static String getStringFromBIS(ByteArrayOutputStream bos) {
         ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
         int c;
